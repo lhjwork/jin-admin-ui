a8b22ff44e793230f2afdaf7f4da4c851cc88417
lhjwork <jin.come.up.business@gmail.com>
2023-10-20 16:43:57 +0900

    7:46

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./producs.scss";
 const Products = () => {
-  return <div>Products</div>;
+  return <div className="products">Products</div>;
 };
 
 export default Products;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./users.scss";
 const Users = () => {
-  return <div>Users</div>;
+  return <div className="users">Users</div>;
 };
 
 export default Users;
