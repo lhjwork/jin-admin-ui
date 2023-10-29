b7fb19690292ff874bc56114bdaf281a08247e2f
lhjwork <jin.come.up.business@gmail.com>
2023-10-29 22:16:17 +0900

    react-router-dom set-up and layout 잡기 login page 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import Home from "./pages/home/Home";
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/users", element: <Users /> },
+        { path: "/products", element: <Products /> },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./menu.scss";
+
+const Menu = () => {
+  return <div className="menu">Menu</div>;
+};
+
+export default Menu;
