f655836fda0ddd4a49b14fbef56d677264e4905a
lhjwork <jin.come.up.business@gmail.com>
2023-10-30 17:44:25 +0900

    React Menu Design 32:00

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>jin-admin</span>
+      <span>© jin Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./menu.scss";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="listItem">
+          <img src="/home.svg" alt="" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/" className="listItem">
+          <img src="/profile.svg" alt="" />
+          <span className="listItemTitle">Profile</span>
+        </Link>
+      </div>
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="listItem">
+          <img src="/home.svg" alt="" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/" className="listItem">
+          <img src="/profile.svg" alt="" />
+          <span className="listItemTitle">Profile</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
