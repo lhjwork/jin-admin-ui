4261289ab5c1a8b8ad634d4258a973f5210a1405
lhjwork <jin.come.up.business@gmail.com>
2023-11-07 14:50:34 +0900

    ChartBox end 10457

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./home.scss";
 import TopBox from "../../components/topBox/TopBox";
 import ChartBox from "../../components/chartBox/ChartBox";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 const Home = () => {
   return (
     <div className="home">
@@ -9,18 +15,18 @@ const Home = () => {
         <TopBox />
       </div>
       <div className="box box2">
-        <ChartBox />
+        <ChartBox {...chartBoxUser} />
       </div>
       <div className="box box3">
-        <ChartBox />
+        <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">
-        <ChartBox />
-      </div>
+      <div className="box box4"></div>
       <div className="box box5">
-        <ChartBox />
+        <ChartBox {...chartBoxConversion} />
       </div>
-      <div className="box box6">Box6</div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
       <div className="box box9">Box9</div>
