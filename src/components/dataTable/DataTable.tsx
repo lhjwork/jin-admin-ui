3dbf2559a7a39f3cfdeab9fd553ab8e683d6bae2
lhjwork <jin.come.up.business@gmail.com>
2023-11-09 15:14:32 +0900

    DataGrid field actions add

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -10,45 +10,69 @@ import {
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
+    field: "avatar",
+    headerName: "프로필",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
   },
   {
-    field: "lastName",
-    headerName: "Last name",
+    field: "actions",
+    headerName: "관리",
+    width: 100,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <div className="view">View</div>
+          <div className="delete">Delete</div>
+        </div>
+      );
+    },
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 100,
+    type: "boolean",
+  },
+  {
+    field: "name",
+    headerName: "이름",
     width: 150,
     editable: true,
   },
   {
     field: "age",
-    headerName: "Age",
+    headerName: "나이",
     type: "number",
     width: 110,
+    renderCell: (params) => {
+      return params.value || "ㅡ";
+    },
     editable: true,
   },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
+  // {
+  //   field: "fullName",
+  //   headerName: "Full name",
+  //   description: "This column has a value getter and is not sortable.",
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params: GridValueGetterParams) =>
+  //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  // },
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, name: "Snow Jon", age: 35, status: true },
+  { id: 2, name: "이해준", age: 42, status: false },
+  { id: 3, name: "Lannister Jaime", age: 45 },
+  { id: 4, name: "김상준", age: 16 },
+  { id: 5, name: "Targaryen", age: null },
+  { id: 6, name: "Melisandre", age: 150 },
+  { id: 7, name: "김동원", age: 44 },
+  { id: 8, name: "Frances", age: 36 },
+  { id: 9, name: "김명진", age: 65 },
 ];
 const DataTable = () => {
   return (
@@ -75,6 +99,9 @@ const DataTable = () => {
         pageSizeOptions={[5]}
         checkboxSelection
         disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
       />
     </div>
   );
