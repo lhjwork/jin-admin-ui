3691804abd1ac4620e49b21e1ee7152d96c4ebad
lhjwork <jin.come.up.business@gmail.com>
2023-11-13 11:51:03 +0900

    chart data map 21159

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -10,104 +10,69 @@ import {
 } from "recharts";
 import "./single.scss";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+type Props = {
+  id: number;
+  img?: string;
+  title: string;
+  info: object;
+  chart?: {
+    datakeys: { name: string; color: string }[];
+    data: object[];
+  };
+  activities?: { time: string; text: string };
+};
 
-const Single = () => {
+const Single = (props: Props) => {
   return (
     <div className="single">
       <div className="view">
         <div className="info">
           <div className="topInfo">
-            <img src="https://source.unsplash.com/random" alt="" />
-            <h1>John Doe</h1>
+            {props.img && <img src={props.img} alt="" />}
+            <h1>{props.title}</h1>
             <button>Update</button>
           </div>
           <div className="details">
-            <div className="item">
-              <div className="itemTitle">Username:</div>
-              <div className="itemValue">John Doe</div>
-            </div>
-            <div className="item">
-              <div className="itemTitle">Username:</div>
-              <div className="itemValue">John Doe</div>
-            </div>
-            <div className="item">
-              <div className="itemTitle">Username:</div>
-              <div className="itemValue">John Doe</div>
-            </div>
+            {Object.entries(props.info).map((item) => (
+              <div className="item" key={item[0]}>
+                <div className="itemTitle">{item[0]}</div>
+                <div className="itemValue">{item[1]}</div>
+              </div>
+            ))}
           </div>
         </div>
         <hr />
-        <div className="chart">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+        {props.chart && (
+          <div className="chart">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width={500}
+                height={300}
+                data={props.chart.data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                {props.chart.datakeys.map((dataKey) => (
+                  <Line
+                    type="monotone"
+                    dataKey={dataKey.name}
+                    stroke={dataKey.color}
+                    activeDot={{ r: 8 }}
+                  />
+                ))}
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        )}
       </div>
       <div className="activities">
         <h2>최신 활동들</h2>
