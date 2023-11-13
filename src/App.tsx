bd38056083b90455f0cca3008f2d8f664b59aac6
lhjwork <jin.come.up.business@gmail.com>
2023-11-13 23:50:51 +0900

    react-query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,14 @@ import "./styles/global.scss";
 import User from "./pages/user/User";
 import Product from "./pages/product/Product";
 
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   const Layout = () => {
     return (
@@ -26,7 +34,9 @@ function App() {
             <Menu />
           </div>
           <div className="contentContainer">
-            <Outlet />
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
           </div>
         </div>
         <Footer />
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./users.scss";
 import DataTable from "../../components/dataTable/DataTable";
 import { GridColDef } from "@mui/x-data-grid";
 import { userRows } from "../../data";
 import Add from "../../components/add/Add";
+
+import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
@@ -82,13 +84,30 @@ const columns: GridColDef[] = [
 
 const Users = () => {
   const [open, setOpen] = useState(false);
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:8800/api/users");
+      return res.json();
+    },
+  });
+
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
         <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
-      <DataTable slug="users" columns={columns} rows={userRows} />
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <DataTable
+          slug="users"
+          columns={columns}
+          rows={data === undefined ? userRows : data}
+        />
+      )}
       {open && <Add setOpen={setOpen} slug="user" columns={columns} />}
     </div>
   );
